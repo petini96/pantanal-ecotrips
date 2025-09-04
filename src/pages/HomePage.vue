514b522cf7c0fda215179005753071198f1874ae
Vinicius dos Santos Petini <viniciuspetini.isteo@ms.senai.br>
2025-09-04 14:13:14 -0400

    added image

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -122,6 +122,12 @@ import { useMeta } from 'quasar';
 import { useI18n } from 'vue-i18n';
 import { useRoute, useRouter } from 'vue-router';
 
+import image1 from 'src/assets/images/tours/tour_1.jpg'
+import image2 from 'src/assets/images/tours/tour_2.jpeg'
+import image3 from 'src/assets/images/tours/tour_3.jpg'
+import image4 from 'src/assets/images/tours/tour_4.jpg'
+import image5 from 'src/assets/images/tours/tour_5.jpg'
+
 const { t, locale } = useI18n();
 const route = useRoute();
 const router = useRouter();
@@ -209,12 +215,12 @@ onMounted(() => {
 // --- DADOS DOS PASSEIOS ---
 interface Tour { id: string; image: string; price: string; }
 const tours = ref<Tour[]>([
-  { id: 'safari_pantanal', image: 'src/assets/images/tours/tour_1.jpg', price: '2.850,00' },
-  { id: 'flutuacao_prata', image: 'src/assets/images/tours/tour_2.jpeg', price: '1.980,00' },
-  { id: 'combo_pantanal_bonito', image: 'src/assets/images/tours/tour_3.jpg', price: '4.500,00' },
-  { id: 'cavalgada_pantaneira', image: 'src/assets/images/tours/tour_4.jpg', price: '1.550,00' },
-  { id: 'outro_exemplo_1', image: 'src/assets/images/tours/tour_5.jpg', price: '1.234,00' },
-  { id: 'outro_exemplo_2', image: 'src/assets/images/tours/tour_1.jpg', price: '5.678,00' },
+  { id: 'safari_pantanal', image: image1, price: '2.850,00' },
+  { id: 'flutuacao_prata', image: image2, price: '1.980,00' },
+  { id: 'combo_pantanal_bonito', image: image3, price: '4.500,00' },
+  { id: 'cavalgada_pantaneira', image: image4, price: '1.550,00' },
+  { id: 'outro_exemplo_1', image: image5, price: '1.234,00' },
+  { id: 'outro_exemplo_2', image: image1, price: '5.678,00' },
 ]);
 
 // --- FUNÇÕES DE TRADUÇÃO E NAVEGAÇÃO ---
