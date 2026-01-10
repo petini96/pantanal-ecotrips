05875ea7750bb392f3c052c9e39dc9f954057177
Vinicius Dos Santos Petini <vinicius.petini@ms.senai.br>
2026-01-10 04:17:22 -0400

    change var

--- src/utils/environmentUtils.ts
+++ src/utils/environmentUtils.ts
@@ -6,5 +6,5 @@ export const getApiUrl = () => {
 export const GALLERY_BUCKET_NAME = process.env.GALLERY_BUCKET_NAME || 'https://minio-s3.roboticsmind.com.br/general-bucket/gallery';
 export const TOURS_BUCKET_NAME = process.env.TOURS_BUCKET_NAME || 'https://minio-s3.roboticsmind.com.br/tours-bucket';
 export const PACKAGE_BUCKET_NAME = process.env.PACKAGE_BUCKET_NAME || 'https://minio-s3.roboticsmind.com.br/packages-bucket';
-export const GENERAL_BUCKET_NAME = process.env.GENERAL_BUCKET_NAME || 'https://minio-s3.roboticsmind.com.br/general-bucket';
+export const GENERAL_BUCKET_NAME = 'https://minio-s3.roboticsmind.com.br/general-bucket';
 export const REGIONS_BUCKET_NAME = process.env.REGIONS_BUCKET_NAME || 'https://minio-s3.roboticsmind.com.br/regions';
